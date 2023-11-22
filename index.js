fe306f0e2e553eb48c00bae291e352b68cd43057
Manuel Fernández <manuelfn066@gmail.com>
2023-11-22 18:43:39 +0100

    update colors and buttons

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ let url = "https://pokeapi.co/api/v2/pokemon/";
 
 
 function pokemonAleatorio() {
-    let i = Math.floor(Math.random() * 151) + 1;;
+    let i = Math.floor(Math.random() * 1000) + 1;;
     fetch(url + i)
         .then((res) => res.json())
         .then((data) => {
+            
             console.log(data);
             contenedor.innerHTML += `
             <h1>Pokemon Aleatorio</h1>
                 <div class="card">
                     <img src="${data.sprites.front_default}" alt="${data.name} width="200" height="200">
                     <h2>${data.name}</h2>
-                    <h3>${data.types[0].type.name}</h3>
+                    ${data.types.map((type) => `<button class="type ${type.type.name}">${type.type.name}</button>`).join(" ")}
                     <p>#${data.id}</p>
                 </div>
                 <a href="index.html" class="btn">Recargar</a>
             `;
+            
         });
 }
+
+
 
 pokemonAleatorio();
